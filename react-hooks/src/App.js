b69bf8f8b04c71db2839dd53316c4f053cde9966
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-02 22:17:47 +0300

    feat: add BrowserRouter, Routes, Route for elements: Home page, About, Profile

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import {Navbar} from './components/Navbar'
 import {Home} from './pages/Home'
+import {About} from './pages/About'
+import {Profile} from './pages/Profile'
 
 function App() {
   return (
-    <React.Fragment>
-      <Navbar />
-      <div className={'container pt-4'}>
-        <Home />
-      </div>
-    </React.Fragment>
-
+    <BrowserRouter>
+        <Navbar />
+        <div className={'container pt-4'}>
+          <Routes>
+            <Route path='/' exact element={<Home />}/>
+            <Route path='/about' element={<About />}/>
+            <Route path='profile'>
+              <Route path=':name' element={<Profile />}/>
+            </Route>
+          </Routes>
+        </div>
+    </BrowserRouter>
   )
 }
 
