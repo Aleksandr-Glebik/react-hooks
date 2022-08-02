a14448b395fbae33f121da7b007b885d824994ee
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-02 21:23:56 +0300

    init: setup new project; connect bootstrap

---
+++ react-hooks/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className={'container pt-4'}>
+      <h1>Hello! This is my new project - react-hooks</h1>
+    </div>
+  )
+}
+
+export default App;
