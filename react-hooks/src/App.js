0c63616d31376b221559e31c0d8581dae038976b
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-03 00:18:47 +0300

    feat: add github... Context, State, Reducers

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -6,24 +6,28 @@ import {About} from './pages/About'
 import {Profile} from './pages/Profile'
 import { Alert } from './components/Alert'
 import { AlertState } from './context/alert/AlertState'
+import { GithubState } from './context/github/githubState'
 
 function App() {
   return (
-    <AlertState>
-      <BrowserRouter>
-          <Navbar />
-          <div className={'container pt-4'}>
-            <Alert alert={{text: 'Test alert'}}/>
-            <Routes>
-              <Route path='/' exact="true" element={<Home />}/>
-              <Route path='/about' element={<About />}/>
-              <Route path='profile'>
-                <Route path=':name' element={<Profile />}/>
-              </Route>
-            </Routes>
-          </div>
-      </BrowserRouter>
-    </AlertState>
+    <GithubState>
+      <AlertState>
+        <BrowserRouter>
+            <Navbar />
+            <div className={'container pt-4'}>
+              <Alert alert={{text: 'Test alert'}}/>
+              <Routes>
+                <Route path='/' exact="true" element={<Home />}/>
+                <Route path='/about' element={<About />}/>
+                <Route path='profile'>
+                  <Route path=':name' element={<Profile />}/>
+                </Route>
+              </Routes>
+            </div>
+        </BrowserRouter>
+      </AlertState>
+    </GithubState>
+
   )
 }
 
