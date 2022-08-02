a14372192b69487365340dc16c70c6d1634bbd0d
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-02 23:44:12 +0300

    feat: add component alert

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -4,21 +4,26 @@ import {Navbar} from './components/Navbar'
 import {Home} from './pages/Home'
 import {About} from './pages/About'
 import {Profile} from './pages/Profile'
+import { Alert } from './components/Alert'
+import { AlertState } from './context/alert/AlertState'
 
 function App() {
   return (
-    <BrowserRouter>
-        <Navbar />
-        <div className={'container pt-4'}>
-          <Routes>
-            <Route path='/' exact="true" element={<Home />}/>
-            <Route path='/about' element={<About />}/>
-            <Route path='profile'>
-              <Route path=':name' element={<Profile />}/>
-            </Route>
-          </Routes>
-        </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+          <Navbar />
+          <div className={'container pt-4'}>
+            <Alert alert={{text: 'Test alert'}}/>
+            <Routes>
+              <Route path='/' exact="true" element={<Home />}/>
+              <Route path='/about' element={<About />}/>
+              <Route path='profile'>
+                <Route path=':name' element={<Profile />}/>
+              </Route>
+            </Routes>
+          </div>
+      </BrowserRouter>
+    </AlertState>
   )
 }
 
