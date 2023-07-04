ecf94dc0a1074d9b183bf8972d1f0c57bf7749b8
Aleksandr-Glebik <glebikaa@mail.ru>
2023-07-04 14:30:46 +0300

    feat: i sass, add style for nav in header

--- react-hooks/src/components/Navbar.js
+++ react-hooks/src/components/Navbar.js
@@ -2,18 +2,18 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => (
-    <nav className={'navbar navbar-dark bg-primary navbar-expand-lg'}>
+    <nav className={'navbar navbar-dark bg-primary navbar-expand-lg px-5 d-flex'}>
         <div className={'navbar-brand'}>
             Github Поиск
         </div>
-        <ul className={'navbar-nav'}>
-            <li className={'nav-item'}>
+        <ul className={'navbar-nav d-flex flex-row flex-grow-1'}>
+            <li className={'nav-item p-2'}>
                 <NavLink to="/" exact="true" className={'nav-link'}>Главная</NavLink>
             </li>
-            <li className={'nav-item'}>
+            <li className={'nav-item p-2'}>
                 <NavLink to="/about" className={'nav-link'}>Информация</NavLink>
             </li>
-            <li className={'nav-item'}>
+            <li className={'nav-item p-2'}>
                 <NavLink to="/profile/aleksandr-glebik" className={'nav-link'}>Профиль</NavLink>
             </li>
         </ul>
